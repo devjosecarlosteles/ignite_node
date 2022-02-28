7caea10a435ddbc65fe26f85c7090275d6fe50fa
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-02-27 22:09:46 -0300

    [fundamento-node] get courses

--- fundamentos_node/index.js
+++ fundamentos_node/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+app.get("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"])
 });
 
 app.listen(3333, () => console.log("server running in http://localhost:3333"));
