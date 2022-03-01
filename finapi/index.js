737536c3a06fe3db39c0bbb964ff8d5f5401c196
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-03-01 10:30:27 -0300

    [finapi] receber dados

--- finapi/index.js
+++ finapi/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
-const uuid = require("uuid")
+const { v4: uuidv4 } = require("uuid")
 
 const app = express()
+
+app.use(express.json())
+
+const customers = []
 
 /** 
   cpf - string
@@ -11,6 +15,17 @@ const app = express()
 */
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body
+
+  const id = uuidv4()
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+
+  return res.status(201).send()
 })
 
 app.listen(3333, () => console.log("server running in localhost:3333"))
