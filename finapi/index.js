f2c2e60e80661f81ab6ade0be7f992c9cbb8de9e
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-03-01 10:16:47 -0300

    [finapi] destruct cpf and name

--- finapi/index.js
+++ finapi/index.js
@@ -9,7 +9,7 @@ const app = express()
   statement []
 */
 app.post("/account", (req, res) => {
-  const cpf = req.body.cpf
+  const { cpf, name } = req.body
 })
 
 app.listen(3333, () => console.log("server running in localhost:3333"))
