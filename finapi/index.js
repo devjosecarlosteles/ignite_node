e8da304eceb98d1445c6c3ec386cb297cd86ca80
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-03-05 17:43:29 -0300

    [finapi] middlewares

--- finapi/index.js
+++ finapi/index.js
@@ -7,12 +7,20 @@ app.use(express.json())
 
 const customers = []
 
-/** 
-  cpf - string
-  name - string
-  id - uuid
-  statement []
-*/
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+  
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ err: "Customer not found" })
+  }
+
+  req.customer = customer
+
+  return next()
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body
 
@@ -32,14 +40,8 @@ app.post("/account", (req, res) => {
   return res.status(201).send()
 })
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params
-
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return res.status(400).json({ err: "Customer not found" })
-  }
+app.get("/statement", verifyIfExistsAccountCPF,(req, res) => {
+  const { customer } = req
 
   return res.json(customer.statement)
 })
