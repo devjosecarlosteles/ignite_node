0838673ffb52c7cff06df79bd13214a0d967f815
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-03-06 00:09:31 -0300

    update account

--- finapi/index.js
+++ finapi/index.js
@@ -109,4 +109,14 @@ app.get("/statement/date", verifyIfExistsAccountCPF,(req, res) => {
   return res.json(statement)
 })
 
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { name } = req.body
+  const { customer } = req
+
+  customer.name = name
+
+  return res.status(201).send()
+})
+
+
 app.listen(3333, () => console.log("server running in localhost:3333"))
